8131682c95120e8e55b97020b8e151140afda4cf
Angel <angelbansal2002@gmail.com>
2022-08-04 22:15:07 +0530

    hehe

---
+++ assignment1/q2.cpp
@@ -0,0 +1,10 @@
+/*) Design the logic to remove the duplicate elements from an Array and after the
+deletion the array should contain the uniquee elements*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
